2e54610cf1096c168e0cfb0b749463edde31be16
jorgejaujr <mrpeaceman@gmail.com>
2019-01-26 14:29:29 -0200

    Added methods destroy and redo in the class

--- mainPanel4.py
+++ mainPanel4.py
@@ -7,50 +7,14 @@ from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
 import tkinter
 
-def on_key_press(event):
-    print("you pressed {}".format(event.key))
-    key_press_handler(event, canvas, toolbar)
 
 
-def _quit():
-    root.quit()     # stops mainloop
-    root.destroy()  # this is necessary on Windows to prevent
+
+
                     # Fatal Python Error: PyEval_RestoreThread: NULL tstate
-def destroy():
-    canvas.get_tk_widget().destroy()
 
-def redo():
-    destroy()
-    step=float(e1.get())
-    f=interpolate.interp1d(arrayX,arrayY,kind='linear')
-    """Este metodo gera uma funcao para a interpolacao linear"""
 
-    #interpolacao polinomial por lagrange - f1
-    f1=interpolate.lagrange(arrayX,arrayY)
-    """Este metodo gera polinomios para a interpolacao baseados nos pontos dados"""
 
-    #interpolacao de spline Cubico - f2
-    f2=interpolate.CubicSpline(arrayX,arrayY)
-    """Este metodo gera polinomios para a interpolacao baseados nos pontos dados"""
-
-    xnew=np.arange(p1.getX(),p3.getX(),step)
-    ynew=f(xnew)
-    ynew1=f1(xnew)
-    ynew2=f2(xnew)
-
-    fig = Figure(figsize=(5, 4), dpi=100)
-    fig.add_subplot(111).plot(xnew, ynew,"ro")
-    fig.add_subplot(111).plot(xnew, ynew1,"b--")
-    fig.add_subplot(111).plot(xnew, ynew2,"g^")
-
-    canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
-    canvas.draw()
-    canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
-
-    toolbar = NavigationToolbar2Tk(canvas, root)
-    toolbar.update()
-    canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
-    canvas.mpl_connect("key_press_event", on_key_press)
     
 step=0.1
 
@@ -106,7 +70,7 @@ class Gui:
         self.toolbar = NavigationToolbar2Tk(self.canvas, master)
         self.toolbar.update()
         self.canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
-        self.canvas.mpl_connect("key_press_event", on_key_press)
+        self.canvas.mpl_connect("key_press_event", self.on_key_press)
 
         self.var1=tkinter.IntVar()
         self.var2=tkinter.IntVar()
@@ -119,9 +83,9 @@ class Gui:
         self.chkbox3.pack(side=tkinter.BOTTOM)
 
 
-        self.button = tkinter.Button(master=master, text="Quit", command=_quit)
+        self.button = tkinter.Button(master=master, text="Quit", command=master.quit)
         self.button.pack(side=tkinter.BOTTOM)
-        self.button1 = tkinter.Button(master=master, text="Pega passo", command=redo)
+        self.button1 = tkinter.Button(master=master, text="Pega passo", command=self.redo)
         self.button1.pack(side=tkinter.BOTTOM)
 
         self.e1=tkinter.Entry(master)
@@ -129,6 +93,49 @@ class Gui:
 
         self.lb1=tkinter.Label(master,text="Passo de Spline")
         self.lb1.pack()
+   
+   def redo(self):
+        self.destroy()
+        step=float(self.e1.get())
+        f=interpolate.interp1d(arrayX,arrayY,kind='linear')
+        """Este metodo gera uma funcao para a interpolacao linear"""
+
+        #interpolacao polinomial por lagrange - f1
+        f1=interpolate.lagrange(arrayX,arrayY)
+        """Este metodo gera polinomios para a interpolacao baseados nos pontos dados"""
+
+        #interpolacao de spline Cubico - f2
+        f2=interpolate.CubicSpline(arrayX,arrayY)
+        """Este metodo gera polinomios para a interpolacao baseados nos pontos dados"""
+
+        xnew=np.arange(p1.getX(),p3.getX(),step)
+        ynew=f(xnew)
+        ynew1=f1(xnew)
+        ynew2=f2(xnew)
+
+        fig = Figure(figsize=(5, 4), dpi=100)
+        fig.add_subplot(111).plot(xnew, ynew,"ro")
+        fig.add_subplot(111).plot(xnew, ynew1,"b--")
+        fig.add_subplot(111).plot(xnew, ynew2,"g^")
+
+        canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+        toolbar = NavigationToolbar2Tk(canvas, root)
+        toolbar.update()
+        canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+        canvas.mpl_connect("key_press_event", self.on_key_press)
+   
+   def on_key_press(self,event):
+        print("you pressed {}".format(event.key))
+        self.key_press_handler(self.event, self.canvas, self.toolbar)
+
+   def destroy(self):
+        self.canvas.get_tk_widget().destroy()
+   #def _quit():
+   #    self.master.quit()     # stops mainloop
+   #    self.master.destroy()  # this is necessary on Windows to prevent
 
 root = tkinter.Tk()
 gui=Gui(root)
