0ba97089b71e52aaf88707bcfddd9ef2bb4dce0f
jorgejaujr <jorgejaujr@gmail.com>
2019-12-03 06:48:57 -0300

    Add files via upload
    Updated tk window with cubic spline, measures and 1s.hex

--- mainPanel3.py
+++ mainPanel3.py
@@ -34,13 +34,13 @@ def redo():
     """Este metodo gera polinomios para a interpolacao baseados nos pontos dados"""
 
     xnew=np.arange(p1.getX(),p3.getX(),step)
-    ynew=f(xnew)
-    ynew1=f1(xnew)
+#    ynew=f(xnew)
+#    ynew1=f1(xnew)
     ynew2=f2(xnew)
 
     fig = Figure(figsize=(5, 4), dpi=100)
-    fig.add_subplot(111).plot(xnew, ynew,"ro")
-    fig.add_subplot(111).plot(xnew, ynew1,"b--")
+#    fig.add_subplot(111).plot(xnew, ynew,"ro")
+#    fig.add_subplot(111).plot(xnew, ynew1,"b--")
     fig.add_subplot(111).plot(xnew, ynew2,"g^")
 
     canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
@@ -52,38 +52,84 @@ def redo():
     canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
     canvas.mpl_connect("key_press_event", on_key_press)
     
-step=0.05
+#importacao do arquivo com as temperaturas
+data = open('201910292240measures107.txt','r')
+
+#criacao do array de temperaturas tirando o nextLine
+array=data.read().splitlines()
+
+#criacao da lista para inserir os segundos e as temperaturas
+pointVec=[]
+
+#inicio dos segundos em float, devido a classe Point
+sec=0.0
+
+#insercao das instancias de Point (segundo,temperatura) a partir do vetor de temperaturas
+#e do contador sec 
+for i in range(0,len(array)):
+   
+   temp = point.Point(float(sec),float(array[i]))
+   pointVec.append(temp)
+   sec+=1
+
+#fechamento do arquivo
+data.close()
+
+#passo para criar os pontos
+step=5.0
 
 root = tkinter.Tk()
 root.wm_title("Embedding in Tk")
+#criacao dos arrays de numpy para x e y
+temp3X=[]
+temp3Y=[]
 
-p3 = point.Point(3.0,4.0)
-p2 = point.Point(2.0,3.0)
-p1 = point.Point(1.0,2.0)
-
-arrayX = np.array([p1.getX(),p2.getX(),p3.getX()])
-arrayY = np.array([p1.getY(),p2.getY(),p3.getY()])
+for i in pointVec:
+  temp3X.append(i.getX())
+  temp3Y.append(i.getY())
+arrayX=np.array(temp3X)
+arrayY=np.array(temp3Y)
+#print("Array Y:",len(arrayY))
 
 f=interpolate.interp1d(arrayX,arrayY,kind='linear')
 """Este metodo gera uma funcao para a interpolacao linear"""
 
 #interpolacao polinomial por lagrange - f1
-f1=interpolate.lagrange(arrayX,arrayY)
+#f1=interpolate.lagrange(arrayX,arrayY)
 """Este metodo gera polinomios para a interpolacao baseados nos pontos dados"""
 
 #interpolacao de spline Cubico - f2
-f2=interpolate.CubicSpline(arrayX,arrayY)
+#f2=interpolate.CubicSpline(arrayX,arrayY)
 """Este metodo gera polinomios para a interpolacao baseados nos pontos dados"""
 
-xnew=np.arange(p1.getX(),p3.getX(),step)
+#xnew=np.arange(p1.getX(),p3.getX(),step)
+xnew=np.arange(pointVec[0].getX(),pointVec[len(pointVec)-1].getX(),step)
+
+ynewSpline=[]
+for k in range(0,len(pointVec),int(step)):
+    ynewSpline.append(pointVec[k].getY())
+
+#ynewSpline=np.arange(pointVec[0].getY(),pointVec[len(pointVec)-1].getY(),step)
+print(len(xnew)," ",len(ynewSpline))
+#array de step segundos, interpolacao linear 
 ynew=f(xnew)
-ynew1=f1(xnew)
-ynew2=f2(xnew)
+
+#ynew1=f1(xnew)
+
+#Spline para cada step segundos
+f2=interpolate.CubicSpline(xnew,ynewSpline)
+ynew2=f2(arrayX)
+print("Y spline cubico: ",len(ynew2))
+#diffs = arrayY - ynew2
+
+#print(numpy.amax(diffs))
+#print(numpy.amin(diffs))
 
 fig = Figure(figsize=(5, 4), dpi=100)
-fig.add_subplot(111).plot(xnew, ynew,"ro")
-fig.add_subplot(111).plot(xnew, ynew1,"b--")
-fig.add_subplot(111).plot(xnew, ynew2,"g^")
+fig.add_subplot(111).plot(arrayX,arrayY,"ro")
+fig.add_subplot(111).plot(xnew, ynew,"b^")
+#fig.add_subplot(111).plot(xnew, ynew1,"b--")
+fig.add_subplot(111).plot(arrayX, ynew2,"g--")
 
 canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
 canvas.draw()
@@ -116,9 +162,4 @@ lb1=tkinter.Label(root,text="Passo de Spline")
 lb1.pack()
 
 tkinter.mainloop()
-# aqui terminam as alteracores
-# basta descomentar esta parte do codigo que volta ao normal.
-"""
-ptl.plot(arrayX,arrayY,"ro",xnew,ynew,"b--",xnew,f2(xnew),"g^",xnew,f2(xnew,1),"r--",xnew,f1(xnew),"go")
-ptl.show()
-"""
+
